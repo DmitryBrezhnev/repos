2459dda5a2a5c1a8959e27f23d27f270bd2ad23e
dmibre <dmibre@greeny.cs.tlu.ee>
2016-02-18 12:30:53 +0200

    Added this functionality to the app

--- app.php
+++ app.php
@@ -1,5 +1,25 @@
 <?
 
+ //check if there is variable in the URL
+ if(isset($_GET["message"])){
+	  
+	//only if there is message in the URL
+    //echo "there is mesaage";
+
+    //if its empty	
+	 echo "there is message";
+	 
+	 
+	 if(emty($_GET["message"])){
+		 echo "Please, enter the message!";
+		 //it is empty
+	 }else{
+		 //its not empty
+		 echo "Message: ".$_GET["message"]."<br>";
+	 }
+ 
+ }else{
+	 //echo "there is no such thing as message"
 
 //getting the message from address
 // if there is ?name= .. then $_GET["name"]
