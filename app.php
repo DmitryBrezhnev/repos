23d2d792c868d8312f67e6ca1d68a8a7b53ef404
dmibre <dmibre@greeny.cs.tlu.ee>
2016-02-18 11:05:01 +0200

    Added this functionality to the app

---
+++ app.php
@@ -0,0 +1,15 @@
+<?
+
+
+
+?>
+
+<h2> First application </h2>
+
+<form method="get">
+   <label for="message">Message:* <label><br>
+   <input name="message" type="text"><br>
+   <input name="submit" value="Save to DB">
+   
+<form>
+bla bal
